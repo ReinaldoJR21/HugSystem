9c84decaec52eac2f00ef2b19517440576a69332
Reinaldo Júnior <104633348+ReinaldoJR21@users.noreply.github.com>
2022-12-24 21:12:02 -0300

    Add files via upload
    Primeira alteração.

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+let boxUm = document.querySelector(".form__container.primeiro");
+let boxDois = document.querySelector(".form__container.segundo");
+
+function passarDeBox() {
+    boxUm.classList.toggle("hide");
+    boxDois.classList.toggle("show");
+}
